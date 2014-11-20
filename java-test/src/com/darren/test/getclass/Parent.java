cae8f81f5a121c776e85ce42b28fb91a463af287
zhangpanfeng <sf0902@163.com>
2014-11-20 17:25:24 +0800

    delete
    Signed-off-by: zhangpanfeng <sf0902@163.com>

--- java-test/src/com/darren/test/getclass/Parent.java
+++
@@ -1,5 +0,0 @@
-package com.darren.test.getclass;
-
-public class Parent {
-
-}
--- java-test/src/com/darren/test/getclass/Son.java
+++
@@ -1,8 +0,0 @@
-package com.darren.test.getclass;
-
-public class Son extends Parent {
-
-    public String testGetClass() {
-        return super.getClass().getName();
-    }
-}
